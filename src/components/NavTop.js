7e86924038dacec3a8e17bd015e483d2af50240c
DEEPAK <90988442+DeathRay@users.noreply.github.com>
2022-09-20 16:13:14 +0530

    first commit

---
+++ src/components/NavTop.js
@@ -0,0 +1,16 @@
+import './NavTop.css';
+function NavTop(){
+    return(
+        <div className="navTop">
+        <div>
+          <a href="">Admission</a>
+          <a href="">Placement</a>
+          <a href="">Alumuni</a>
+          <a href="">Contact</a>
+          <a href="">Research</a>
+          <a href="">Blog</a>
+          </div>
+        </div>
+    )
+}
+export default NavTop;
