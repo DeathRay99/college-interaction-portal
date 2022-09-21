d57e9cdb38f7a76ff398c150151eaa8d71451db0
amar <upadhyayamar2420@gmail.com>
2022-09-22 02:29:16 +0530

    adding footer day1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import NavTop from './components/NavTop';
 import NavTop2 from './components/NavTop2';
 import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,7 @@ function App() {
       <NavTop2/>
       <Carousel/>
       </div>
+       <Footer/>
     </div>
   );
 }
