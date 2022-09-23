c54e4d06a8fde6b6b0601ab5b1d25ce54876c479
amar <upadhyayamar2420@gmail.com>
2022-09-23 14:02:23 +0530

    seventh-commit

--- src/components/NavTop2.js
+++ src/components/NavTop2.js
@@ -1,8 +1,18 @@
 import './NavTop2.css';
+import { useEffect, useState } from "react";
 import jiitLogo from './assets/jiitLogo.png';
 function NavTop2(){
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 400) {
+        setShow(true);
+      } else setShow(false);
+    });
+  }, []);
     return(
-        <div className="navTop2">
+        <div className="navTop2"
+      style={{ backgroundColor: show ? "#212529" : "transparent" }}>
          <div className="logodiv">
            <img  className="logo" src={jiitLogo}  alt="" />
          </div>
