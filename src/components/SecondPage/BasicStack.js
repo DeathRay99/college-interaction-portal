cdd6887b959e378d2908917626daee6bf1d3d4d2
DEEPAK <90988442+DeathRay@users.noreply.github.com>
2022-09-25 00:57:07 +0530

    eigth  commit

--- src/components/SecondPage/BasicStack.js
+++ src/components/SecondPage/BasicStack.js
@@ -1,35 +1,40 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
-import AboutUs from './AboutUs'
-import Placements from './Placements'
-import { styled } from '@mui/material/styles';
-import './BasicStack.css'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import AboutUs from "./AboutUs";
+import Placements from "./Placements";
+import { styled } from "@mui/material/styles";
+import "./BasicStack.css";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(9),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
 
 export default function BasicStack() {
   return (
-    <Box sx={{ width: '100%' }}>
+    <Box sx={{ width: "100%" }}>
       <Stack spacing={1}>
         <div className="second-page">
           <Item>
-            <AboutUs/>
-            </Item>
-            </div>
+            <AboutUs />
+          </Item>
+        </div>
         <div className="placements">
-          <Item>
-            <Placements/>
-            </Item>
-            </div>
-       
+          <Item
+            sx={{
+              padding: 0,
+              paddingTop: "70px",
+              paddingBottom: "70px",
+            }}
+          >
+            <Placements />
+          </Item>
+        </div>
       </Stack>
     </Box>
   );
--- src/components/SecondPage/PlacementOverview.js
+++ src/components/SecondPage/PlacementOverview.js
@@ -1,31 +1,32 @@
-import React, { useState } from 'react'
-import CountUp from 'react-countup';
-import ScrollTrigger from 'react-scroll-trigger';
+import React, { useState } from "react";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
 
-
-
-function PlacementOverview () {
-const [ counterOn, setCounterOn] = useState(false);
+function PlacementOverview() {
+  const [counterOn, setCounterOn] = useState(false);
   return (
-    <ScrollTrigger onEnter={()=>setCounterOn(true)}
-    onExit={()=> setCounterOn(false)}>
-    <div>PlacementOverview
-    Recruiters Visited
-    <h1>
-        
-     {counterOn&&<CountUp start={0} end={160} duration={3} delay={1}/>}
-    </h1>
-    Students Placed
-    <h1>
-        
-     {counterOn&&<CountUp start={0} end={1000} duration={3} delay={1}/>}+</h1>
-     Average CTC
-    <h1>
-        
-     {counterOn&&<CountUp start={0} end={11} duration={3} delay={1}/>}LPA</h1>
-    </div>
+    <ScrollTrigger
+      onEnter={() => setCounterOn(true)}
+      onExit={() => setCounterOn(false)}
+    >
+      <div>
+        PlacementOverview Recruiters Visited
+        <h1>
+          {counterOn && <CountUp start={0} end={160} duration={3} delay={1} />}
+        </h1>
+        Students Placed
+        <h1>
+          {counterOn && <CountUp start={0} end={1000} duration={3} delay={1} />}
+          +
+        </h1>
+        Average CTC
+        <h1>
+          {counterOn && <CountUp start={0} end={11} duration={3} delay={1} />}
+          LPA
+        </h1>
+      </div>
     </ScrollTrigger>
-  )
+  );
 }
 
-export default PlacementOverview
+export default PlacementOverview;
