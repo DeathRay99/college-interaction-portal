e3b0c604324a71c6fbace3669dd449c6bbce4c44
DEEPAK <90988442+DeathRay@users.noreply.github.com>
2022-09-29 10:34:19 +0530

    deepak suicide

--- src/components/ApplyNow.js
+++ src/components/ApplyNow.js
@@ -1,48 +1,55 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import React, { useState } from 'react'
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
-
 const theme = createTheme();
 
-export default function SignUp() {
+export default function ApplyNow() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
+  const [stream,setStream] = useState(0);
 
+  const handleChange = (event) => {
+    setStream(event.target.value);
+  };
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
@@ -50,18 +57,23 @@ export default function SignUp() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Apply Now
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -82,6 +94,9 @@ export default function SignUp() {
                   label="Last Name"
                   name="lastName"
                   autoComplete="family-name"
+                  sx={{
+                    border: "5px solid black"
+                  }}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -105,13 +120,9 @@ export default function SignUp() {
                   autoComplete=""
                 />
               </Grid>
-         
-                
-              <Grid container spacing={2}>
-             
-              <Grid item xs={12} sm={6}>
+
+              <Grid item xs={6}>
                 <TextField
-                margin="dense"
                   required
                   fullWidth
                   name="state"
@@ -123,7 +134,6 @@ export default function SignUp() {
               </Grid>
               <Grid item xs={11} sm={6}>
                 <TextField
-                  margin="dense"
                   required
                   fullWidth
                   name="city"
@@ -133,8 +143,7 @@ export default function SignUp() {
                   autoComplete=""
                 />
               </Grid>
-              </Grid>
-      
+
               <Grid item xs={12}>
                 <div></div>
                 <TextField
@@ -148,23 +157,24 @@ export default function SignUp() {
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
-
-  <Select  required
+              <InputLabel>Branch*</InputLabel>
+                <Select
+                  disableUnderline
+                  required
                   fullWidth
-    labelId="demo-simple-select-label"
-    id="demo-simple-select"
-    
-    label="Age"
-   
-  >
-    <MenuItem value={10}>CSE</MenuItem>
-    <MenuItem value={20}>ECE</MenuItem>
-   
-  </Select>
-  </Grid>
+                  value={stream}
+        onChange={handleChange}
+                >
+                  <MenuItem value={0}>None</MenuItem>
+                  <MenuItem value={1}>ECE</MenuItem>
+                  <MenuItem value={2}>CSE</MenuItem>
+                </Select>
+              </Grid>
               <Grid item xs={12}>
                 <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
                   label="I agree to terms and conditions required during admissions and during tenure of exams.."
                 />
               </Grid>
@@ -179,9 +189,6 @@ export default function SignUp() {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
               </Grid>
             </Grid>
           </Box>
@@ -191,3 +198,10 @@ export default function SignUp() {
     </ThemeProvider>
   );
 }
+
+
+
+
+
+
+
