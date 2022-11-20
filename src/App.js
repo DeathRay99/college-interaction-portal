b0c466a6197e371e728f2acdaf7d6b8f4ab1a476
DEEPAK <90988442+DeathRay@users.noreply.github.com>
2022-11-20 11:35:51 +0530

    Ptani bhai Kuch toh kiya hai abb

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ApplyNow from "./components/NewApplicant/ApplyNow";
 import HomePage from "./components/Home/HomePage";
 import NavTop from "./components/Navigation/NavTop";
 import LoginPage from "./components/WebPortal/LoginPage";
 function App() {
   return (
-    <Router>
+    <div>
+      <NavTop/>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="applyNow" element={<div>
-        <NavTop/>
-        <ApplyNow/>
-        </div>}
-        />
-        <Route path="portalHome" element ={<LoginPage/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="applyNow" element={<ApplyNow />} />
+        <Route path="portalHome" element={<LoginPage />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 export default App;
