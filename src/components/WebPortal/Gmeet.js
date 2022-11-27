92310e57801d2c495cf0fca0e293496ce149c4eb
amar <upadhyayamar2420@gmail.com>
2022-11-27 23:20:24 +0530

    27-11-2022 second

---
+++ src/components/WebPortal/Gmeet.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import Button from '@mui/material/Button';
+export default function ActionAreaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Google_Meet.max-1100x1100.png"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          <Button variant="contained" href="#contained-buttons">
+        Create Meet 
+      </Button>
+
+          </Typography>
+         
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
--- src/components/WebPortal/Widgets.js
+++ src/components/WebPortal/Widgets.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Widgets.css"
 import Button from '@mui/material/Button';
-
+import ActionAreaCard from './Gmeet';
 
 function Widgets() {
   return (
@@ -13,6 +13,9 @@ function Widgets() {
       </div>
       <div className='widgets__widgetcontainer'>
       <h2>Whats Happening </h2>
+      <div className='gmeet'>
+      <ActionAreaCard />
+      </div>
       
      
       </div>
