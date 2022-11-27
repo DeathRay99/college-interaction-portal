2cf15eb86abbfebb53e1cab4114b994df3bb6031
amar <upadhyayamar2420@gmail.com>
2022-11-27 22:45:17 +0530

    27-11-2022

--- src/components/WebPortal/Post.js
+++ src/components/WebPortal/Post.js
@@ -3,9 +3,8 @@ import "./Post.css";
 import { Avatar } from '@mui/material';
 import VerifiedIcon from '@mui/icons-material/Verified';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import RepeatIcon from '@mui/icons-material/Repeat';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import PublishIcon from '@mui/icons-material/Publish';
+
 import SimplePaper from './Commentsection';
 
 function Post({
